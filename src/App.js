49922fb0158e89651e67ba92004bbdd2c329cd94
Sumalya Chatterjee <105884682+R3DHULK@users.noreply.github.com>
2023-06-12 15:25:19 +0530

    Calculator App

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import './Calculator.css';
+
+const Calculator = () => {
+  const [display, setDisplay] = useState('');
+
+  const handleButtonClick = (value) => {
+    setDisplay((prevDisplay) => prevDisplay + value);
+  };
+
+  const handleClearClick = () => {
+    setDisplay('');
+  };
+
+  const handleCalculate = () => {
+    try {
+      const result = eval(display);
+      setDisplay(result.toString());
+    } catch (error) {
+      setDisplay('Error');
+    }
+  };
+
+  const handleBackspaceClick = () => {
+    setDisplay((prevDisplay) => prevDisplay.slice(0, -1));
+  };
+
+  return (
+    <div className="calculator">
+      <div className="display">{display}</div>
+      <div className="buttons">
+        <button onClick={() => handleButtonClick('7')}>7</button>
+        <button onClick={() => handleButtonClick('8')}>8</button>
+        <button onClick={() => handleButtonClick('9')}>9</button>
+        <button onClick={() => handleButtonClick('+')}>+</button>
+        <button onClick={() => handleButtonClick('4')}>4</button>
+        <button onClick={() => handleButtonClick('5')}>5</button>
+        <button onClick={() => handleButtonClick('6')}>6</button>
+        <button onClick={() => handleButtonClick('-')}>-</button>
+        <button onClick={() => handleButtonClick('1')}>1</button>
+        <button onClick={() => handleButtonClick('2')}>2</button>
+        <button onClick={() => handleButtonClick('3')}>3</button>
+        <button onClick={() => handleButtonClick('*')}>*</button>
+        <button onClick={() => handleButtonClick('0')}>0</button>
+        <button onClick={() => handleButtonClick('.')}>.</button>
+        <button onClick={handleCalculate}>=</button>
+        <button onClick={() => handleButtonClick('/')}>/</button>
+        <button onClick={handleBackspaceClick} className="backspace-button">
+          Backspace
+        </button>
+        <button onClick={handleClearClick} className="clear-button">
+          Clear
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Calculator;
